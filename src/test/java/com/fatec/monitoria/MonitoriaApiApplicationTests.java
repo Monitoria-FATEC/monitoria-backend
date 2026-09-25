89c2d6ff98073180111f332af80b6ef59f12fe87
deysemedeiros <deysebmsoares@gmail.com>
2026-09-25 12:29:08 -0300

    feat: estrutura inicial da API monitoria com Spring Boot e MongoDB

---
+++ src/test/java/com/fatec/monitoria/MonitoriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.monitoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoriaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
